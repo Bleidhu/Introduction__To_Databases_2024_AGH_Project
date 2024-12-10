8f23fa8d41e975d782d362a232e23da592a3a95f
Szymon Rybski <ichigosr2014@gmail.com>
2024-12-10 16:02:58 +0100

    add skeleton of data generator for courses

--- utils/dummy_data_generator/dummy_data_generator.py
+++ utils/dummy_data_generator/dummy_data_generator.py
@@ -6,7 +6,9 @@ import dummy_values as dval
 USERS_LIMIT = 10
 EMPLOYEES_LIMIT = 10
 COURSES_LIMIT = 10
+COURSE_MODULES_LIMIT = 10
 COURSE_MODULE_MEETINGS_LIMIT = 10
+COURSE_STUDENTS_LIMIT = 10
 CITIES_AMOUNT = len(dval.cities)
 COUNTRIES_AMOUNT = len(dval.countries)
 LANGUAGES_AMOUNt = len(dval.languages)
@@ -38,6 +40,28 @@ def generate_employees_table():
     #translators =  look for translators and append them here
     return tmp
 
+def generate_meeting(id, course_id, course_start_date, previous_meeting_date):
+    # add dates so they won't overlap
+    meeting = db_model.CourseModuleMeeting(course_id, id, fk.date(), "Stationary", random.randint(0, LANGUAGES_AMOUNt), None, random.randint(0, EMPLOYEES_LIMIT),
+                                           "90min", 10)
+    
+    return meeting
+
+def generate_module(id, module_meetings_size, course_start_date):
+    module = db_model.CourseModule(id, 0, "Stationary")
+
+    module_meetings = []
+
+    meetings_amount = random.randint(1, COURSE_MODULE_MEETINGS_LIMIT)
+    last_meeting_date = course_start_date
+    for i in range(meetings_amount + 1):
+        meeting = generate_meeting(i+module_meetings_size, course_start_date, last_meeting_date)
+        last_meeting_date = meeting.meeting_date
+        module_meetings.append(meeting)
+    
+    return module, module_meetings
+
+
 def generate_course(id):
     modules = []
     meetings = []
@@ -46,6 +70,15 @@ def generate_course(id):
     sync_async_meetings = []
     meetings_atendance_list = []
 
+    course = db_model.Course(id, fk.catch_phrase(), "Opis", fk.date_this_century(), random.randint(1, COURSE_STUDENTS_LIMIT), random.random()*100, random.randint(0,EMPLOYEES_LIMIT))
+
+    module_amount = random.randint(1, COURSE_MODULES_LIMIT)
+
+    for i in range(module_amount+1):
+        module = 
+        meetings.append()
+
+    return course
 
     
 
