13557d114d8c955f6e0a005068eb72f53614b442
Szymon Rybski <ichigosr2014@gmail.com>
2024-12-10 15:47:07 +0100

    added person generation

--- utils/dummy_data_generator/dummy_data_generator.py
+++ utils/dummy_data_generator/dummy_data_generator.py
@@ -2,12 +2,14 @@
 import db_tables_classes as db_model
 from faker import Faker
 import random
+import dummy_values as dval
 USERS_LIMIT = 10
 EMPLOYEES_LIMIT = 10
 COURSES_LIMIT = 10
 COURSE_MODULE_MEETINGS_LIMIT = 10
-CITIES_AMOUNT = 10
-COUNTRIES_AMOUNT = 10
+CITIES_AMOUNT = len(dval.cities)
+COUNTRIES_AMOUNT = len(dval.countries)
+LANGUAGES_AMOUNt = len(dval.languages)
 
 fk = Faker()
 
@@ -21,14 +23,19 @@ def generate_users_table():
 
     return tmp
 
+def generate_role_id():
+    return 0
+
 def generate_employee(id):
-    tmp = db_model.Employee(id,  fk.first_name(), fk.last_name(), fk.date_of_birth(minimum_age=18, maximum_age=60), fk.date_this_decade() ,fk.phone(), fk.email(), 0,random.randint(0, CITIES_AMOUNT), random.randint(0, COUNTRIES_AMOUNT))
+    tmp = db_model.Employee(id,  fk.first_name(), fk.last_name(), fk.date_of_birth(minimum_age=18, maximum_age=60), fk.date_this_decade() ,fk.phone_number(), fk.email(), generate_role_id(),random.randint(0, CITIES_AMOUNT), random.randint(0, COUNTRIES_AMOUNT))
+
+    
 
     return tmp
 
 def generate_employees_table():
     tmp = [generate_employee(i) for i in range(EMPLOYEES_LIMIT)]
-
+    #translators =  look for translators and append them here
     return tmp
 
 def generate_course(id):
