9a10447f45942de529e26c98f8945b85eec78541
Szymon Rybski <ichigosr2014@gmail.com>
2024-12-10 20:27:48 +0100

    still working it's hard

--- utils/dummy_data_generator/dummy_data_generator.py
+++ utils/dummy_data_generator/dummy_data_generator.py
@@ -15,8 +15,11 @@ LANGUAGES_AMOUNt = len(dval.languages)
 
 fk = Faker()
 
+def generate_email_from_name(first_name: str, last_name: str):
+    return first_name.lower() + last_name.lower() + "@" + fk.domain_name()
 def generate_user(id):
-    tmp = db_model.User(id, fk.email(), fk.first_name(), fk.last_name(), random.randint(0, CITIES_AMOUNT), random.randint(0, COUNTRIES_AMOUNT))
+    name, last_name = fk.first_name(), fk.last_name()
+    tmp = db_model.User(id, generate_email_from_name(name, last_name), name, last_name, random.randint(0, CITIES_AMOUNT), random.randint(0, COUNTRIES_AMOUNT))
 
     return tmp
 
@@ -62,7 +65,7 @@ def generate_module(id, module_meetings_size, course_start_date):
     return module, module_meetings
 
 
-def generate_course(id, modules_table_size, module_meetings_table_size):
+def generate_course(id, modules_table_size, module_meetings_table_size, enrolled_students_size, stationary_meetings_size, sync_async_meetings_size):
     modules = []
     meetings = []
     enrolled_students = []
@@ -75,11 +78,17 @@ def generate_course(id, modules_table_size, module_meetings_table_size):
     module_amount = random.randint(1, COURSE_MODULES_LIMIT)
 
     for i in range(module_amount+1):
-        module, tmp_meetings = generate_module(i+modules_table_size, module_meetings_table_size, course.start_date)
+        module, tmp_meetings, tmp_meeting_attendance_list, tmp_stationary_meetings, tmp_sync_async_meetings, tmp_meeting_attendance_list = generate_module(i+modules_table_size, 
+        module_meetings_table_size, course.start_date, enrolled_students_size + len(enrolled_students)
+        , stationary_meetings_size + len(stationary_meetings), sync_async_meetings_size + len(sync_async_meetings))
         modules.append(module)
         meetings += tmp_meetings
+        stationary_meetings += tmp_stationary_meetings
+        sync_async_meetings += tmp_sync_async_meetings
+        stationary_meetings += tmp_stationary_meetings
+        meetings_atendance_list += tmp_meeting_attendance_list
 
-    return course, modules, meetings
+    return course, modules, meetings, enrolled_students, stationary_meetings, sync_async_meetings, meetings_atendance_list
 
 def generate_courses_table():
     modules_all = []
@@ -87,23 +96,27 @@ def generate_courses_table():
     enrolled_students_all = []
     sync_async_meeting_all = []
     meetings_atendace_list_all = []
+    stationary_meetings = []
 
     courses = []
     for i in range(COURSES_LIMIT):
-        tmp_course, tmp_modules, tmp_meetings, tmp_enrolled_students, tmp_stationary_meetings, tmp_sync_async_meetings, tmp_meetings_attendance_list = generate_course(i, len(modules_all), len(meetings_all), len(enrolled_students_all), len(sync_async_meeting_all))
+        tmp_course, tmp_modules, tmp_meetings, tmp_enrolled_students, tmp_stationary_meetings, tmp_sync_async_meetings, tmp_meetings_attendance_list = generate_course(i, len(modules_all), len(meetings_all), len(enrolled_students_all), len(stationary_meetings), len(sync_async_meeting_all))
 
         modules_all += tmp_modules
         meetings_all += tmp_meetings
         enrolled_students_all += tmp_enrolled_students 
         sync_async_meeting_all += tmp_sync_async_meetings
         meetings_atendace_list_all += tmp_meetings_attendance_list
+        
+        courses.append(tmp_course)
     
 
 
 def main():
     users = generate_users_table()
+    print("INSERT INTO Users (user_id, email, first_name, last_name, city_id, country_id, phone, street, house_number)")
     for user in users:
-        print(user)
+        print("(" + str(user) + ")")
 
     employees = generate_employees_table()
     for employee in employees:
